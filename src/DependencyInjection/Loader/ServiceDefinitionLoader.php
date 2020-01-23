7935db5709af6b7efd57cff2a7462e740929f0eb
moay <mv@moay.de>
2020-01-23 14:45:36 +0100

    Fixes service definition

--- src/DependencyInjection/Loader/ServiceDefinitionLoader.php
+++ src/DependencyInjection/Loader/ServiceDefinitionLoader.php
@@ -40,42 +40,33 @@ class ServiceDefinitionLoader
         $definition = (new Definition())
             ->setClass(PageRouter::class)
             ->setPublic(true);
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(PageRouterInterface::class, $definition);
 
         $definition = (new Definition())
             ->setFactory('@'.LogFileDatabaseFactory::class);
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(LogFileDatabase::class, $definition);
 
         $definition = (new Definition())
             ->setClass(Environment::class)
             ->setFactory('@'.TwigEnvironmentFactory::class);
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(Environment::class, $definition);
 
         $definition = (new Definition())
             ->setClass(Logger::class)
             ->setFactory('@'.CronLoggerFactory::class);
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition('logger.cron', $definition);
 
         $definition = (new Definition())
             ->setPublic(true)
             ->setArgument('$cronJobs', $containerBuilder->findTaggedServiceIds('plugin.cron_job'));
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(CronJobRunner::class, $definition);
 
         $definition = (new Definition())
             ->setArgument('$controllers', $containerBuilder->findTaggedServiceIds('plugin.controller'));
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(ControllerExecutor::class, $definition);
 
         $definition = (new Definition())
             ->setArgument('$extensions', $containerBuilder->findTaggedServiceIds('twig.extension'));
-        $definitionsToAdd[] = $definition;
         $containerBuilder->setDefinition(TwigEnvironmentFactory::class, $definition);
-
-        $containerBuilder->addDefinitions($definitionsToAdd);
     }
 }
